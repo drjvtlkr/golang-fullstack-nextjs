1b5ff9628fa8a8b4b06a849bc694fce2a4319168
dheeraj <dheerajrvithalkar@gmail.com>
2024-10-10 00:45:47 +0530

    golang with postgresql implementation done

--- types.go
+++ types.go
@@ -1,20 +1,29 @@
 package main
 
-import "math/rand"
+import (
+	"math/rand"
+	"time"
+)
 
-type Account struct {
-	ID        int    `json:id`
-	FirstName string `json:firstname`
-	LastName  string `json:lastname`
-	Number    int64  `json:number`
-	Balance   int64  `json:balance`
+type CreateAccountRequest struct{
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
 }
 
-func NewAccount(firstname, lastname string) *Account {
+type Account struct {
+	ID        int    	`json:"id"`
+	FirstName string 	`json:"firstName"`
+	LastName  string 	`json:"lastName"`
+	Number    int64  	`json:"number"`
+	Balance   int64  	`json:"balance"`
+	CreatedAt time.Time `json:"createdAt"`
+}
+
+func NewAccount(firstName, lastName string) *Account {
 	return &Account{
-		ID:        rand.Intn(1000000000),
-		FirstName: firstname,
-		LastName:  lastname,
-		Number:    int64(rand.Intn(1000000000)),
+		FirstName: 	firstName,
+		LastName:  	lastName,
+		Number:    	int64(rand.Intn(1000000000)),
+		CreatedAt:	time.Now().UTC(),
 	}
 }
